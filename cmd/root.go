00ff6726c95f595b62f6910878bf9883753eb6f2
vs4vijay <vs4vijay@gmail.com>
2020-03-23 16:09:49 +0530

    Remove defatul run for root command

--- cmd/root.go
+++ cmd/root.go
@@ -34,10 +34,6 @@ func newRootCmd(out io.Writer, in io.Reader) *cobra.Command {
 		Use:   "vega",
 		Short: rootDesc,
 		Long:  rootDesc,
-		Run: func(cmd *cobra.Command, args []string) {
-			// TODO: What to do by default
-			// fmt.Println("root command called")
-		},
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
 			if flagDebug {
 				log.SetLevel(log.DebugLevel)
