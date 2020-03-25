e5a6fce38e88eda0bd780504bb8b2e42010f1539
WSMathias <wsmsamuel@gmail.com>
2020-03-25 10:41:25 +0530

    Update staterkit command

--- cmd/starterkits.go
+++ cmd/starterkits.go
@@ -9,15 +9,10 @@ import (
 func newStarterKitCmd(out io.Writer) *cobra.Command {
 	const starterkitsDesc = "Manage starter-kits used for initializeing projects"
 	starterkitsCmd := &cobra.Command{
-		Use:   "starterkits",
+		Use:   "starterkit",
 		Short: starterkitsDesc,
 		Long:  starterkitsDesc,
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				cmd.Help()
-				// os.Exit(0)
-			}
-		}}
+	}
 	starterkitsCmd.AddCommand(newStarterKitListCmd(out))
 	return starterkitsCmd
 }
